7634db0a1cc542f1f8c367217405297a513aaf71
Duncan Klemm <duncanklemm@outlook.com>
2025-05-02 15:11:05 -0400

    move months to sub-directory

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const jan = [];
-const feb = [];
-const mar = [];
-const apr = [];
-const may = [];
-const jun = [];
-const jul = [];
-const aug = [];
-const sep = [];
-const oct = [];
-const nov = [];
-const dec = [];
+import jan from "/jokes/january.json";
+import feb from "/jokes/february.json";
+import mar from "/jokes/march.json";
+import apr from "/jokes/april.json";
+import may from "/jokes/may.json";
+import jun from "/jokes/june.json";
+import jul from "/jokes/july.json";
+import aug from "/jokes/august.json";
+import sep from "/jokes/september.json";
+import oct from "/jokes/october.json";
+import nov from "/jokes/november.json";
+import dec from "/jokes/december.json";
 const year = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec];
 
 function dailyJoke(){
